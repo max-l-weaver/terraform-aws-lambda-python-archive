49e340e9a15f179b087e7f7038a3004971abf9be
max-l-weaver <maxlweaver@gmail.com>
2021-05-28 11:09:36 +0100

    linter isn't working so missed some issues

--- scripts/build_lambda.py
+++ scripts/build_lambda.py
@@ -26,7 +26,7 @@ def build(src_dir, output_path):
                  '--ignore-installed',
                  '--target', build_dir,
                  '-r', os.path.join(build_dir, 'requirements.txt')],
-                 check=True)
+                 check=True
             )
         make_archive(build_dir, output_path)
         return output_path
